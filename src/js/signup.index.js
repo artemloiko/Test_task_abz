d01fb5ec7c3a13068d4b863e506c59ddfd7136a0
artuom130 <artuom130@gmail.com>
2018-08-31 19:50:12 +0300

    Added textarea responsiveness and char counter for textarea

--- src/js/signup.index.js
+++ src/js/signup.index.js
@@ -1,1 +1,14 @@
 console.log("Hello from signup.index.js");
+
+(function charCount() {
+  const tarea = document.querySelector(".signUp__textarea");
+  const twrap = document.querySelector(".signUp__input-block--tarea");
+  tarea.addEventListener("input", function() {
+    // console.log(twrap.dataset.charcount);
+    this.style.height = "152px";
+    if (this.offsetHeight !== this.scrollHeight) {
+      this.style.height = this.scrollHeight + 2 + "px";
+    }
+    twrap.dataset.charcount = `(${this.value.length}/1000)`;
+  });
+})();
