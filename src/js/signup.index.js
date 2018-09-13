702e23e877461dec918f3b5e2b7abb9ba604b86d
artuom130 <artuom130@gmail.com>
2018-09-13 21:59:27 +0300

    Big refactoring: majority functions in separate files and added sweetalert to response of the sent form

--- src/js/signup.index.js
+++ src/js/signup.index.js
@@ -1,20 +1,28 @@
+import swal from "sweetalert2";
 import style from "../scss/signup.index.scss";
-import { customSelect } from "./select";
+import { customSelect } from "./signup/select";
+import { overlayLoader } from "./signup/overlayLoader";
+import { deleteInputErr, setInputErr, checkForm } from "./signup/formCheck";
+import "./signup/imgUploader";
+import "./signup/textarea.js";
 
-//custom select with error handling
 (function() {
+  //  FILLING and CREATING custom select _____________________________________________
+
+  //get data for select
   fetch("http://504080.com/api/v1/directories/enquiry-types")
     .then(response => response.json())
-    .then(data => createCustomSelect(data.data))
+    .then(data => fillSelect(data.data))
+    .then(select => createCustomSelect(select))
     .catch(error => {
-      // Sorry, the server did not respond. Trying again
-      console.log("fetch error", error);
-      createCustomSelect(null);
+      console.log("loads enquiry-types fetch error", error);
+      fillSelect(null);
     });
-  const createCustomSelect = function(data) {
+
+  //fill default select
+  const fillSelect = function(data) {
     const nativeSelect = document.getElementById("select");
     const other = document.getElementById("select-other");
-
     //Filling select with received data
     if (data) {
       data.forEach((item, i) => {
@@ -24,7 +32,7 @@ import { customSelect } from "./select";
         nativeSelect.appendChild(option);
       });
     } else {
-      //if can't upload types add text input Enquiry type
+      //if can't upload types show only text input Enquiry type
       nativeSelect.hidden = true;
       other.hidden = false;
       other.required = true;
@@ -33,19 +41,13 @@ import { customSelect } from "./select";
       return;
     }
 
+    return Promise.resolve(nativeSelect);
+  };
+
+  // Creating custom select with errors handling __________________________________________________________
+  function createCustomSelect(nativeSelect) {
+    //creating custom select
     const select = customSelect(nativeSelect);
-
-    //show other on last option
-    nativeSelect.addEventListener("change", function(e) {
-      if (this.selectedIndex === this.options.length - 1) {
-        other.hidden = false;
-        other.required = true;
-      } else {
-        other.hidden = true;
-        other.required = false;
-      }
-    });
-
     // custom select check
     select.addEventListener("blur", function() {
       deleteInputErr(this);
@@ -55,172 +57,92 @@ import { customSelect } from "./select";
         select.classList.add("select--error");
       }
     });
-
-    // hide error on change
-    nativeSelect.addEventListener("change", function(e) {
-      if (nativeSelect.dataset.placeholder) delete nativeSelect.dataset.placeholder;
-      if (select.firstElementChild.innerHTML) {
-        deleteInputErr(this);
-        select.classList.remove("select--error");
-      }
+    //on select option delete error
+    nativeSelect.addEventListener("change", () => {
+      deleteInputErr(select);
+      select.classList.remove("select--error");
     });
-  };
+  }
 })();
 
-//TODO add description
-function setInputErr(input, errText) {
-  const inputWrapper = input.closest("label");
-  if (inputWrapper) {
-    inputWrapper.classList.add("form__input-block--error");
-    inputWrapper.dataset.error = errText;
-  }
-}
-//TODO add description
-function deleteInputErr(input) {
-  const inputWrapper = input.closest("label");
-  if (inputWrapper) inputWrapper.classList.remove("form__input-block--error");
-}
+(function() {
+  const nativeSelect = document.getElementById("select");
+  const other = document.getElementById("select-other");
+  //show other on last option
+  nativeSelect.addEventListener("change", function(e) {
+    if (nativeSelect.dataset.placeholder) delete nativeSelect.dataset.placeholder;
+
+    if (this.selectedIndex === this.options.length - 1) {
+      other.hidden = false;
+      other.required = true;
+    } else {
+      other.hidden = true;
+      other.required = false;
+    }
+  });
+})();
+
 //form validations
 //TODO: and sending...
-function sendData(formData) {
-  console.dir(formData.get("user_name"));
-  const url = "http://504080.com/api/v1/support";
-  fetch(url, {
+
+function sendingData(formData) {
+  const loader = overlayLoader(document.documentElement.scrollTop);
+  loader.show();
+
+  fetch("http://504080.com/api/v1/support", {
     method: "POST",
     body: formData
   })
     .then(response => response.json())
-    .then(response => console.log("Success:", response))
-    .catch(error => console.error("Error:", error));
+    .then(response => {
+      loader.hide();
+      if (response.success) {
+        swal({
+          type: "success",
+          title: "Success",
+          text: response.data.message,
+          confirmButtonColor: "#87b448"
+        });
+      } else {
+        //TODO: onerror add error to input
+        let errorText = response.error.description;
+        if (response.error.details) {
+          response.error.details.forEach(errItem => {
+            errorText = errorText + "<br/>" + errItem.description;
+          });
+        }
+
+        swal({
+          type: "error",
+          title: response.error.message,
+          html: errorText,
+          confirmButtonColor: "#87b448"
+        });
+      }
+    })
+    .catch(error => {
+      console.error("Error:", error);
+      swal({
+        type: "error",
+        title: "Error",
+        text: "Sorry, the server did not respond.",
+        confirmButtonColor: "#87b448"
+      });
+    });
 }
-(function() {
-  const form = document.forms[0];
-  //forms elements
-  const select = document.getElementById("select");
-  const other = document.getElementById("select-other");
-  const name = form.elements["user_name"];
-  const email = form.elements["email"];
-  const subject = form.elements["subject"];
-  const description = form.elements["description"];
-  const file = form.elements["file"];
 
-  const emailRegExp = /^[\w.!#$%&'*+/=?^`{|}~-]+@[\w-]+\.[a-zA-Z0-9-]+$/;
-
-  // This field is required on blur
-  const formRequiredElems = [other, name, email, subject, description];
-
-  formRequiredElems.forEach(elem => {
-    elem.addEventListener("blur", function() {
-      checkValueMiss(elem);
-    });
-    elem.addEventListener("input", function() {
-      if (!this.validity.valueMissing) {
-        deleteInputErr(this);
-      }
-    });
-  });
-  // Email check
-  email.addEventListener("blur", function() {
-    deleteInputErr(this);
-    if (!emailRegExp.test(this.value)) {
-      setInputErr(this, "Invalid email");
-    }
-  });
-
-  // Check form
-  document.forms[0].addEventListener("submit", function(e) {
-    formRequiredElems.forEach(elem => checkValueMiss(elem));
-
-    if (!emailRegExp.test(email.value)) {
-      setInputErr(email, "Invalid email");
-      e.preventDefault();
-      return;
-    }
-
-    if (select.dataset.placeholder) {
-      select.nextElementSibling.classList.add("select--error");
-      setInputErr(select, "This field is required");
-      e.preventDefault();
-      return;
-    }
-
-    let formData = new FormData(form);
-    if (other.hidden) {
-      formData.set("enquiry_type", select.options[select.selectedIndex].innerHTML);
-    }
-    sendData(formData);
+document.forms[0].addEventListener("submit", function(e) {
+  if (!checkForm()) {
     e.preventDefault();
-  });
-
-  function checkValueMiss(elem) {
-    deleteInputErr(elem);
-    if (elem.validity.valueMissing) {
-      setInputErr(elem, "This field is required");
-    }
+    return;
   }
-})();
 
-//img uploader with error handling
+  const other = document.getElementById("select-other");
+  let formData = new FormData(document.forms[0]);
+  if (other.hidden) {
+    formData.set("enquiry_type", select.options[select.selectedIndex].innerHTML);
+  }
 
-(function() {
-  const inputFile = document.forms[0].elements["file"];
-  const uploadBlock = inputFile.parentNode;
-
-  inputFile.addEventListener("change", function() {
-    deleteInputErr(this);
-    const f = this.files[0];
-
-    if (!f) return;
-
-    //type file check
-    if (!f.type.match(/image\/\w*/)) {
-      setInputErr(inputFile, "Image only please");
-      return;
-    }
-
-    //size file check
-    if (f.size > 5 * 1024 * 1024) {
-      setInputErr(inputFile, "Image should not exceed 5MB");
-      return;
-    }
-    //dimensions file check
-    //create images with file src to check dimensions
-    const image = document.createElement("img");
-    image.src = window.URL.createObjectURL(f);
-    image.style.position = "fixed";
-    image.style.visibility = "hidden";
-    image.style.pointerEvents = "none";
-    document.forms[0].appendChild(image);
-
-    const reader = new FileReader();
-
-    reader.onload = function(e) {
-      //if dimensions are exceed 300x300 set error
-      if (image.offsetHeight > 300 || image.offsetWidth > 300) {
-        setInputErr(inputFile, "Image should not exceed 300x300 pixels");
-        document.forms[0].removeChild(image);
-        return;
-      }
-      //show uploaded image
-      uploadBlock.classList.add("form__file-wrapper--loaded");
-      uploadBlock.style.backgroundImage = `url(${e.target.result})`;
-    };
-
-    reader.readAsDataURL(f);
-  });
-})();
-
-// textarea charCount
-
-(function() {
-  const tarea = document.querySelector(".form__textarea");
-  const twrap = document.querySelector(".form__input-block--tarea");
-
-  tarea.addEventListener("input", function() {
-    this.style.height = "152px";
-    if (this.offsetHeight !== this.scrollHeight) {
-      this.style.height = this.scrollHeight + 2 + "px";
-    }
-    twrap.dataset.charcount = `(${this.value.length}/1000)`;
-  });
-})();
+  sendingData(formData);
+  e.preventDefault();
+});
