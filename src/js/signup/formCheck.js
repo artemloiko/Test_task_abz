582ffbc4aa9bbafb12b645b955937c36d39888f7
artuom130 <artuom130@gmail.com>
2018-09-13 21:53:28 +0300

    Big refactoring: checkForm and set/deleteInputError in separate module

---
+++ src/js/signup/formCheck.js
@@ -0,0 +1,76 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const form = document.forms[0];
+  //forms elements
+  const other = document.getElementById("select-other");
+  const name = form.elements["user_name"];
+  const email = form.elements["email"];
+  const subject = form.elements["subject"];
+  const description = form.elements["description"];
+
+  const formRequiredElems = [other, name, email, subject, description];
+
+  // Check required fields on blur and input
+  formRequiredElems.forEach(elem => {
+    elem.addEventListener("blur", function() {
+      checkValueMiss(elem);
+    });
+
+    elem.addEventListener("input", function() {
+      if (!this.validity.valueMissing) {
+        deleteInputErr(this);
+      }
+    });
+  });
+
+  // Email check
+  email.addEventListener("blur", function() {
+    deleteInputErr(this);
+    if (!this.validity.valid) {
+      setInputErr(this, "Invalid email");
+    }
+  });
+});
+
+function checkValueMiss(elem) {
+  deleteInputErr(elem);
+  if (elem.validity.valueMissing) {
+    setInputErr(elem, "This field is required");
+  }
+}
+//TODO add description
+export function setInputErr(input, errText) {
+  const inputWrapper = input.closest("label");
+  if (inputWrapper) {
+    inputWrapper.classList.add("form__input-block--error");
+    inputWrapper.dataset.error = errText;
+  }
+}
+
+//TODO add description
+export function deleteInputErr(input) {
+  const inputWrapper = input.closest("label");
+  if (inputWrapper) inputWrapper.classList.remove("form__input-block--error");
+}
+
+// Check form
+export function checkForm() {
+  const select = document.getElementById("select");
+  const email = document.forms[0].elements["email"];
+
+  const formRequiredElems = [...document.forms[0].elements].filter(elem => elem.required);
+  console.log(formRequiredElems);
+  formRequiredElems.forEach(elem => checkValueMiss(elem));
+
+  let formIsCorrect = formRequiredElems.every(elem => !elem.validity.valueMissing);
+  if (!email.validity.valid) {
+    setInputErr(email, "Invalid email");
+    formIsCorrect = false;
+  }
+  if (select.dataset.placeholder) {
+    select.nextElementSibling.classList.add("select--error");
+    setInputErr(select, "This field is required");
+    formIsCorrect = false;
+  }
+
+  return formIsCorrect;
+}
