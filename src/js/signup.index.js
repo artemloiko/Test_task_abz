0d128f2e9daa10faee82b8497eafdb5a0700927d
artuom130 <artuom130@gmail.com>
2018-09-14 19:11:44 +0300

    Put filling select in separate file, changed overlayLoader call

--- src/js/signup.index.js
+++ src/js/signup.index.js
@@ -1,92 +1,13 @@
 import swal from "sweetalert2";
 import style from "../scss/signup.index.scss";
-import { customSelect } from "./signup/select";
 import { overlayLoader } from "./signup/overlayLoader";
 import { deleteInputErr, setInputErr, checkForm } from "./signup/formCheck";
-import "./signup/imgUploader";
-import "./signup/textarea.js";
-
-(function() {
-  //  FILLING and CREATING custom select _____________________________________________
-
-  //get data for select
-  fetch("http://504080.com/api/v1/directories/enquiry-types")
-    .then(response => response.json())
-    .then(data => fillSelect(data.data))
-    .then(select => createCustomSelect(select))
-    .catch(error => {
-      console.log("loads enquiry-types fetch error", error);
-      fillSelect(null);
-    });
-
-  //fill default select
-  const fillSelect = function(data) {
-    const nativeSelect = document.getElementById("select");
-    const other = document.getElementById("select-other");
-    //Filling select with received data
-    if (data) {
-      data.forEach((item, i) => {
-        const option = document.createElement("option");
-        option.value = i;
-        option.innerHTML = item.name;
-        nativeSelect.appendChild(option);
-      });
-    } else {
-      //if can't upload types show only text input Enquiry type
-      nativeSelect.hidden = true;
-      other.hidden = false;
-      other.required = true;
-      other.placeholder = "Enquiry type";
-      other.style.marginTop = 0;
-      return;
-    }
-
-    return Promise.resolve(nativeSelect);
-  };
-
-  // Creating custom select with errors handling __________________________________________________________
-  function createCustomSelect(nativeSelect) {
-    //creating custom select
-    const select = customSelect(nativeSelect);
-    // custom select check
-    select.addEventListener("blur", function() {
-      deleteInputErr(this);
-      select.classList.remove("select--error");
-      if (!select.firstElementChild.innerHTML) {
-        setInputErr(this, "This field is required");
-        select.classList.add("select--error");
-      }
-    });
-    //on select option delete error
-    nativeSelect.addEventListener("change", () => {
-      deleteInputErr(select);
-      select.classList.remove("select--error");
-    });
-  }
-})();
-
-(function() {
-  const nativeSelect = document.getElementById("select");
-  const other = document.getElementById("select-other");
-  //show other on last option
-  nativeSelect.addEventListener("change", function(e) {
-    if (nativeSelect.dataset.placeholder) delete nativeSelect.dataset.placeholder;
-
-    if (this.selectedIndex === this.options.length - 1) {
-      other.hidden = false;
-      other.required = true;
-    } else {
-      other.hidden = true;
-      other.required = false;
-    }
-  });
-})();
-
-//form validations
-//TODO: and sending...
-
+import "./signup/IIFE/uploader";
+import "./signup/IIFE/textarea";
+import "./signup/IIFE/select";
+//Sending data via POST
 function sendingData(formData) {
-  const loader = overlayLoader(document.documentElement.scrollTop);
+  const loader = overlayLoader(document.querySelector(".loader"));
   loader.show();
 
   fetch("http://504080.com/api/v1/support", {
@@ -108,10 +29,10 @@ function sendingData(formData) {
         let errorText = response.error.description;
         if (response.error.details) {
           response.error.details.forEach(errItem => {
+            setInputErr(document.forms[0].elements[errItem.field], errItem.description);
             errorText = errorText + "<br/>" + errItem.description;
           });
         }
-
         swal({
           type: "error",
           title: response.error.message,
