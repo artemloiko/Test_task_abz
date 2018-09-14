87167b3ac93efeec34252b15951901f01321a301
artuom130 <artuom130@gmail.com>
2018-09-14 19:07:31 +0300

    Redesigned loader, now it preventScroll instead scrolling back

--- src/js/signup/overlayLoader.js
+++ src/js/signup/overlayLoader.js
@@ -1,19 +1,50 @@
-const overlayLoader = function(curScroll) {
-  const loaderElem = document.querySelector(".loader");
-  // const curScroll = document.documentElement.scrollTop;
+// left: 37, up: 38, right: 39, down: 40,
+// spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
+const keys = [32, 33, 34, 35, 36, 37, 38, 39, 40];
 
-  function blockScroll() {
-    scrollTo(0, curScroll);
+function preventDefault(e) {
+  e.preventDefault();
+  return false;
+}
+
+function preventDefaultForScrollKeys(e) {
+  if (e.keyCode === 32 || e.keyCode === 13) {
+    e.stopPropagation();
+    e.stopImmediatePropagation();
+    e.preventDefault();
+    return false;
   }
+  if (keys.indexOf(e.keyCode)) {
+    e.preventDefault();
+    return false;
+  }
+}
 
+function disableScroll() {
+  window.addEventListener("DOMMouseScroll", preventDefault, false);
+  window.onwheel = preventDefault; // modern standard
+  window.onmousewheel = document.onmousewheel = preventDefault; // older browsers, IE
+  window.ontouchmove = preventDefault; // mobile
+  document.onkeydown = preventDefaultForScrollKeys;
+}
+
+function enableScroll() {
+  window.removeEventListener("DOMMouseScroll", preventDefault, false);
+  window.onmousewheel = document.onmousewheel = null;
+  window.onwheel = null;
+  window.ontouchmove = null;
+  document.onkeydown = null;
+}
+
+const overlayLoader = function(loader) {
   function showLoader() {
-    loaderElem.style.visibility = "visible";
-    document.addEventListener("scroll", blockScroll);
+    loader.style.visibility = "visible";
+    disableScroll();
   }
 
   function hideLoader() {
-    loaderElem.style.visibility = "hidden";
-    document.removeEventListener("scroll", blockScroll);
+    loader.style.visibility = "hidden";
+    enableScroll();
   }
 
   return {
